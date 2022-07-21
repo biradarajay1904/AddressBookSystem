dd92f1fa43f2160b4c6a9c325eb93bc3e2fcdf09
biradarajay1904 <biradarajay04@gmail.com>
2022-07-21 22:04:03 +0530

    modified: created contact

--- ContactDetails.java
+++ ContactDetails.java
@@ -1,5 +1,5 @@
 package com.Bridgelabz;
-import java.util.Scanner;
+
 
 public class ContactDetails {
 
@@ -70,24 +70,7 @@ public class ContactDetails {
 
 
        
-       Scanner sc = new Scanner(System.in);
-       public void createContacts() {
-           System.out.print(" Enter First Name : ");
-           setFirstName(sc.nextLine());
-           System.out.print(" Enter Last Name : ");
-           setLastName(sc.nextLine());
-           System.out.print(" Enter Address : ");
-           setAddress(sc.nextLine());
-           System.out.print(" Enter city : ");
-           setCity(sc.nextLine());
-           System.out.print(" Enter State : ");
-           setState(sc.nextLine());
-           System.out.print(" Enter Zip Code : ");
-           setZip(sc.nextLine());
-           System.out.print(" Enter Phone Number : ");
-           setPhoneNumber(sc.nextLine());
-           
-       }
+       
 
     
    }
