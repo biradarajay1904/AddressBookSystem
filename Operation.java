69cda7c88a37c9f64ac99a0416635b6fded644d1
biradarajay1904 <99815064+biradarajay1904@users.noreply.github.com>
2022-07-28 12:05:56 +0530

    Update Operation.java

--- Operation.java
+++ Operation.java
@@ -59,50 +59,6 @@ public class Operation {
     }
     */
     
-    public void edit() {
-		 System.out.println("Enter the First and Last name of a Person to be edit ");
-		 String fname = scanner.next();
-		 String lname = scanner.next();
-		 String Fname = con.getFirstName();
-		 String Lname = con.getLastName();
-		 if(fname.equalsIgnoreCase(Fname) && lname.equalsIgnoreCase(Lname)) {
-			    System.out.println("Edit First Name :");
-		    	String fName = scanner.next();
-		    	con.setFirstName(fName);
-		    	
-		    	System.out.println("Edit Last Name :");
-		    	String lName = scanner.next();
-		    	con.setLastName(lName);
-		    	
-		    	System.out.println("Edit Address :");
-		    	String address = scanner.next();
-		    	con.setAddress(address);
-		    	
-		    	System.out.println("Edit City :");
-		    	String city = scanner.next();
-		    	con.setCity(city);
-		    	
-		    	System.out.println("Edit State:");
-		    	String state = scanner.next();
-		    	con.setState(state);
-		    	
-		    	System.out.println("Edit Zip Code :");
-		    	String zip = scanner.next();
-		    	con.setZip(zip);
-		    	
-		    	System.out.println("Edit Phone :");
-		    	String phoneno = scanner.next();
-		    	con.setPhoneNumber(phoneno);
-		 }		    	
-		 }
-		//Delete the person Contact
-			public void delete() {
-				System.out.println("Delete the Person");
-				String fname = scanner.next();
-				System.out.println(fname+"");
-				if (fname.equals(con.getFirstName())) {
-						contactDetails.remove(con);
-						System.out.println("contact is Deleted from the Address Book");
-					}
+   
 }
     }
